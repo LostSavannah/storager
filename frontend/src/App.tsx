4060fefe4340f4be77b0df89196e39a3679bb8f3
Erick Fernando Mora Ramirez <erickmoradev@gmail.com>
2024-03-31 22:57:09 -0400

    Adding header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import StorageComponent from "./shared/components/StorageComponent/StorageCompon
 export default function App() {
   return (
     <div>
+      <div className="w-100 h4 bg-dark p-3 text-light">
+        <span className="p-3">Storager</span>
+      </div>
       <StorageComponent/>
     </div>
   )
